125e5d56a9fd2ca9f4f5c4f4136748b35ed69753
user <user@DESKTOP-3G4HHUT>
2022-02-19 17:13:00 -0800

    Add project files.

---
+++ WebApplicationMvc/Migrations/20220219180541_A1.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplicationMvc.Migrations
+{
+    public partial class A1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Phone_No = table.Column<long>(type: "bigint", nullable: false),
+                    Email_id = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    place = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Details = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Locations");
+        }
+    }
+}
---
+++ WebApplicationMvc/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationMvc.Models
+{
+    public class Customer
+    {
+        internal object location;
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public long Phone_No { get; set; }
+
+        public string Email_id { get; set; }
+        public location Location { get; set; }
+    }
+}
